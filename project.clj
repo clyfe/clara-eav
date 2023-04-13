20c607b71d931da37ffb7122a2a23e480632faec
clyfe <>
2023-04-13 14:42:03 +0300

    Release 0.1.8

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clyfe/clara-eav "0.1.8-SNAPSHOT"
+(defproject clyfe/clara-eav "0.1.8"
   :description "EAV triplets for Clara Rules"
   :url         "https://github.com/clyfe/clara-eav"
   :license      {:name "MIT"
